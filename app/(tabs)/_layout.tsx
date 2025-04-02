407653985414ddb1cdf67b81c1d2d62116beabe1
ismail dikici <ismaildikici@tiktakkirala.com>
2025-04-02 21:50:28 +0300

    library base

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -62,6 +62,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <FontAwesome name="user" size={24} color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="library"
+        options={{
+          title: 'Library',
+          tabBarIcon: ({ color }) => <FontAwesome name="user" size={24} color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/library.tsx
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+export default function LibraryScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <Text>LibraryScreen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
